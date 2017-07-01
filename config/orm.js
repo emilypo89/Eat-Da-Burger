ace8fffa727356356a17eda5bc666b842073b559
emilypo89 <emilypo89@gmail.com>
2017-07-01 12:46:21 -0400

    working application

--- config/orm.js
+++ config/orm.js
@@ -1,39 +1,76 @@
-// 3. Create an `orm.js` file inside `config` directory.
+// require `connection.js` into `orm.js`
+var connection = require("../config/connection.js");
+function printQuestionMarks(num) {
+  var arr = [];
 
-//    * Import (require) `connection.js` into `orm.js`
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
 
-//    * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+  return arr.toString();
+}
 
-//      * `selectAll()` 
-//      * `insertOne()` 
-//      * `updateOne()` 
+function objToSql(ob) {
+  var arr = [];
 
-//    * Export the ORM object in `module.exports`.
-var connection = require("./connection.js");
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
 
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+// methods you will need to use in order to retrieve and store data in your database using mySQL commands
 var orm = {
-	selectAll: function(whatToSelect, table, orderCol) {
-    var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
+	selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
     // console.log(queryString);
-    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+    connection.query(queryString, function(err, result) {
     	if (err) throw err;
-      console.log(result);
+      cb(result);
     });
   },
-  insertOne: function(table, colOne, colTwo, colOneVal, colTwoVal){
-  	var queryString = "INSERT INTO ?? (??, ??) VALUES (?, ?)";
-  	connection.query(queryString, [table, colOne, colTwo, colOneVal, colTwoVal], function(err, result){
+  insertOne: function(table, cols, vals, cb){
+  	var queryString = "INSERT INTO " + table;
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+
+      console.log(queryString);
+
+  	connection.query(queryString, vals, function(err, result){
   		if (err) throw err;
-  		console.log(result);
+  		cb(result);
   	});
   },
-  updateOne: function(table, colName, colVal, idVal){
-  	var queryString = "UPDATE ?? SET ?? = ? WHERE id = ?";
-  	connection.query(queryString, [table, colName, colVal, idVal], function(err, result) {
+  updateOne: function(table, objColVals, condition, cb){
+  	var queryString = "UPDATE " + table;
+      queryString += " SET ";
+      queryString += objToSql(objColVals); 
+      queryString += " WHERE ";
+      queryString += condition;
+
+      console.log(queryString);
+  	connection.query(queryString, function(err, result) {
   		if (err) throw err;
-  		console.log(result);
+  		cb(result);
   	});
   }
 }
-
+// Export the ORM object
 module.exports = orm;  
--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,28 @@
-    // * Inside `burger.js`, import `orm.js` into `burger.js`
-
-    // * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
-
-    // * Export at the end of the `burger.js` file.
-
+// import `orm.js` into `burger.js`
 var orm = require("../config/orm.js");
 
-orm.selectAll("*", "burgers", "id");
+// code that will call the ORM functions using burger specific input for the ORM.
+var burgers = {
+	selectAll: function(cb){
+		orm.selectAll("burgers", function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(cols, vals, cb) {
+		orm.insertOne("burgers", cols, vals, function(res) {
+			cb(res);
+		});
+	},
+	updateOne: function(objColVals, condition, cb) {
+		orm.updateOne("burgers", objColVals, condition, function(res){
+			cb(res);
+		});
 
-orm.insertOne("burgers", "burger_name", "devoured", "Turkey Burger", "false");
+	}
 
-orm.updateOne("burgers", "burger_name", "Cheeseburger", 3);
+}
 
 
-module.exports = burgers.js
+
+// Export `burger.js` file.
+module.exports = burgers;
